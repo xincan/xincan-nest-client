2d06fa907f6e4e89e698a5b6993438bc97500888
jiangxincan <jiangxincan@hatech.com.cn>
2019-09-04 12:59:59 +0800

    动态读取菜单，并动态按需加载路由

--- src/components/main/Navigation.vue
+++ src/components/main/Navigation.vue
@@ -16,20 +16,20 @@
         @click="clickParentBtn(nav, key)"
       >
         <!-- 设置一级菜单图标 -->
-        <span><i :class="'fa ' + nav.icon" />&nbsp;&nbsp;{{ nav.name }}</span>
+        <span><i :class="'fa ' + nav.icon" />&nbsp;&nbsp;{{ nav.menuName }}</span>
 
         <!-- 设置一级菜单图标，判断如果菜单展开状态，箭头向上，反之向下 -->
-        <span v-if="nav.child && nav.child.length > 0"><i :class="'fa ' + (key === pKey ? 'fa-angle-up' : 'fa-angle-down')" /></span>
+        <span v-if="nav.children && nav.children.length > 0"><i :class="'fa ' + (key === pKey ? 'fa-angle-up' : 'fa-angle-down')" /></span>
 
         <!-- 设置一级菜单右侧选中‘缺口’图标 点击则有，反之没有 -->
-        <span v-if="key === pKey && nav.child && nav.child.length == 0"><i :class="'fa fa-caret-left caret'" /></span>
+        <span v-if="key === pKey && nav.children && nav.children.length == 0"><i :class="'fa fa-caret-left caret'" /></span>
 
         <!--
              判断是否有二级菜单，如果有则显示，反之则隐藏
         -->
         <div
           :class="'child ' + (key === pKey ? ' is-show' : 'is-hide')"
-          v-if="nav.child && nav.child.length > 0"
+          v-if="nav.children && nav.children.length > 0"
         >
           <ul>
             <!--
@@ -39,10 +39,10 @@
             -->
             <li
               :class="index === cKey ? 'active' : ''"
-              v-for="(child, index) in nav.child"
+              v-for="(child, index) in nav.children"
               @click.stop="clickChildBtn(child, index)"
             >
-              <span><i :class="'fa ' + child.icon" />&nbsp;&nbsp;{{ child.name }}</span>
+              <span><i :class="'fa ' + child.icon" />&nbsp;&nbsp;{{ child.menuName }}</span>
               <span><i :class="'fa ' + (index === cKey ? 'fa-caret-left caret' : '')" /></span>
             </li>
           </ul>
@@ -65,95 +65,45 @@
       return {
         pKey: 0           // 系统默认自动展开选中第一个一级菜单
         ,cKey: 0          // 系统默认自动选中第一个一级菜单下的第一个二级菜单
-        ,menus:[{
-          id: 11
-          ,name: '主页'
-          ,icon: 'fa-home'
-          ,code: 'dashboard'
-          ,path: '/dashboard'
-          ,params: {}
-        },{
-          id: 121
-          ,name: '系统管理'
-          ,icon: 'fa-home'
-          ,code: ''
-          ,path: ''
-          ,params: {}
-          ,child: [
-             {id: 1211 ,name: '地区管理', code:'areaManage',        icon: 'fa-globe',     path: '/components/views/system/manage/AreaManage', params: {}}
-            ,{id: 1212 ,name: '菜单管理', code:'menuManage',        icon: 'fa-bars',      path: '/components/views/system/manage/MenuManage', params: {}}
-            ,{id: 1213 ,name: '用户管理', code:'userManage',        icon: 'fa-user-o',    path: '/components/views/system/manage/UserManage', params: {}}
-            ,{id: 1214 ,name: '角色管理', code:'roleManage',        icon: 'fa-user-md',   path: '/components/views/system/manage/RoleManage', params: {}}
-            ,{id: 1215 ,name: '权限管理', code:'permissionManage',  icon: 'fa-home',      path: '/components/views/system/manage/PermissionManage', params: {}}
-          ]
-        },{
-          id: 131
-          ,name: '预警管理'
-          ,icon: 'fa-home'
-          ,code: ''
-          ,path: ''
-          ,params: {}
-          ,child: [
-            {id: 1311 ,name: '预警编辑', code:'warnManage', icon: 'fa-globe',    path: '/components/views/system/warn/WarnManage', params: {}}
-
-          ]
-        }]
+        ,menus:[]
       }
     }
     ,created(){
-
+      // 初始化加载菜单
+      this.initMenuAndRouters();
     }
     ,mounted(){
 
-      // 初始化加载菜单
-      this.getMenu();
 
 
     }
     ,methods: {
 
-      getMenu(){
+      /**
+       * 初始化菜单和路由
+       *
+       */
+      initMenuAndRouters(){
 
+        this.$get("/api/menu/list", {}).then( response => {
+          let routers = [];
+          this.menus = response.data;
 
-        // this.$post("/api/user/boot", {page:1, limit: 10,sortName:'name', sortType: 'DESC'}).then( response => {
-        //   console.log(response)
-        //
-        // }).catch( error => {console.log(error);});
-
-
-        const router = [{
-          path: '/areaManage',
-          name: 'areaManage',
-          params:{},
-          component: resolve => require(['@/components/views/system/manage/AreaManage.vue'], resolve)
-        }, {
-          path: '/menuManage',
-          name: 'menuManage',
-          params:{},
-          component: resolve => require(['@/components/views/system/manage/MenuManage.vue'], resolve)
-        }, {
-          path: '/userManage',
-          name: 'userManage',
-          params:{},
-          component: resolve => require(['@/components/views/system/manage/UserManage.vue'], resolve)
-        }, {
-          path: '/roleManage',
-          name: 'roleManage',
-          params:{},
-          component: resolve => require(['@/components/views/system/manage/RoleManage.vue'], resolve)
-        }, {
-          path: '/permissionManage',
-          name: 'permissionManage',
-          params:{},
-          component: resolve => require(['@/components/views/system/manage/PermissionManage.vue'], resolve)
-        }, {
-          path: '/warnManage',
-          name: 'warnManage',
-          params:{},
-          component: resolve => require(['@/components/views/system/warn/WarnManage.vue'], resolve)
-        }];
-
-        this.$router.addRoutes(router);
+          for(let menu of response.data) {
+            if(menu.children && menu.children.length > 0) {
+              for(let router of menu.children) {
+                routers.push({
+                  path: '/' + router.code,
+                  name: router.code,
+                  params: JSON.parse(router.params),
+                  // component: (resolve) => require.ensure([], () => resolve (require('@/' + router.path.substring(router.path.indexOf("/") + 1) + '.vue')))
+                  component: resolve => require(['@/' + router.path.substring(router.path.indexOf("/") + 1) + '.vue'], resolve)
+                });
+              }
+            }
+          }
+          this.$router.addRoutes(routers);
+        }).catch( error => {console.log(error);});
       },
 
       /**
@@ -164,11 +114,11 @@
         this.pKey = key;        // 点击一级菜单：改变当前点击菜单的下标
         this.cKey = 0;          // 默认展示第一个二级菜单
 
-          if(menu.child && menu.child.length > 0){
-            this.$router.push({name: menu.child[0].code,params:{name:'默认二级第一个菜单'}}); // 触发路由跳转
-          }else {
-            this.$router.push({name: menu.code,params:{name:'主页'}}); // 触发路由跳转
-          }
+        if(menu.children && menu.children.length > 0){
+          this.$router.push({name: menu.children[0].code, query: JSON.parse(menu.params)}); // 触发路由跳转
+        }else {
+          this.$router.push({name: menu.children, query: JSON.parse(menu.params)});         // 触发路由跳转
+        }
 
       }
 
@@ -177,8 +127,8 @@
        * @Method controlBtn
        */
       ,clickChildBtn(menu, index){
-        this.cKey = index;                    // 点击二级菜单：改变当前点击二级菜单的下标
-        this.$router.push({name: menu.code,params:{name:'二级'}}); // 触发路由跳转
+        this.cKey = index;                                                    // 点击二级菜单：改变当前点击二级菜单的下标
+        this.$router.push({name: menu.code,query: JSON.parse(menu.params)});  // 触发路由跳转
       }
 
     }
