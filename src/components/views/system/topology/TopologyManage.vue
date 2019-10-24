b340f95361e0ce079722ad2a389e008d189fd5b3
mswang <2265936571@qq.com>
2019-10-24 14:01:17 +0800

    拓扑管理

---
+++ src/components/views/system/topology/TopologyManage.vue
@@ -0,0 +1,44 @@
+<!--suppress ALL -->
+<template>
+  <div class="myPage">
+    <div ref="canvasDiv" class="canvasDiv"></div>
+  </div>
+</template>
+
+<script>
+import zrender from 'zrender'
+export default {
+    name: "TopologyManage",
+    data () {
+      return {}
+    },
+    methods:{},
+    mounted (){
+      let zr = zrender.init(this.$refs.canvasDiv);
+      var circle = new zrender.Arc({
+        shape: {
+          cx: 150,
+          cy: 50,
+          r: 40,
+          startAngle:0
+        },
+        style: {
+          fill: 'none',
+          stroke: '#F00'
+        }
+      });
+      zr.add(circle);
+    }
+}
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss">
+  .myPage{
+    width: 100%;
+    height:100%;
+    .canvasDiv {
+      width: 100%;
+      height: calc(100% - 50px);
+    }
+  }
+</style>
